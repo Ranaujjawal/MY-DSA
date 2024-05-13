ec0af66e3ca07a81e4d0b2d3e65ff33450a8ee0f
Ujjawal <ranaujjawal692@gmail.com>
2024-05-13 13:09:04 +0530

    commit

--- pattern/allpatterns.cpp
+++ pattern/allpatterns.cpp
@@ -32,8 +32,33 @@ void nStarTriangle(int n) {
         cout<<"\n";
     }
 }
-
-
+// *         *
+// * *     * *
+// * * * * * *
+// * *     * *
+// *         *
+void symmetry(int n) {
+    // Write your code here.
+    int spaces=2*n-2;
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n) stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"* ";
+        }
+         for(int j=1;j<=spaces;j++){
+            cout<<" ";
+        }
+         for(int j=1;j<=stars;j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+        if(i<n){
+            spaces-=2;
+        }
+        else spaces+=2;
+    }
+}
 
 
 
